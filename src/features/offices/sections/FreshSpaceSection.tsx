d67fcb1111b074048dc5fdad6bd2ab9670f596cb
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2025-10-15 22:00:22 +0700

    Refactor FreshSpaceSection component for improved layout and responsiveness; adjust grid columns for better display on different screen sizes

--- src/features/offices/sections/FreshSpaceSection.tsx
+++ src/features/offices/sections/FreshSpaceSection.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import Image from 'next/image'
-import OfficeSpaceCard from '../components/officeSpaceCard'
-import { officeSpaces } from '../data/officeSpaces.mock'
+import React from 'react';
+import Image from 'next/image';
+import OfficeSpaceCard from '../components/officeSpaceCard';
+import { officeSpaces } from '../data/officeSpaces.mock';
 
 const OfficeSection = () => {
   return (
-          <section id="Fresh-Space" className="flex flex-col gap-[30px] w-full max-w-[1130px] mx-auto mt-[100px] mb-[120px]">
-            <h2 className="font-bold text-[32px] leading-[48px] text-nowrap text-center">
-              Browse Our Fresh Space.
-              <br />
-              For Your Better Productivity.
-            </h2>
-            <div className="grid grid-cols-3 gap-[30px] px-[30px] lg:!px-0">
-              {officeSpaces.map((office) => (
-                <OfficeSpaceCard key={office.id} officeSpace={office} />
-              ))}
-            </div>
-          </section>
-  )
-}
+    <section id="Fresh-Space" className="flex flex-col gap-[30px] w-full max-w-[1130px] mx-auto mt-[100px] mb-[120px]">
+      <h2 className="font-bold text-[32px] leading-[48px] text-nowrap text-center">
+        Browse Our Fresh Space.
+        <br />
+        For Your Better Productivity.
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[30px] px-20 sm:px-10 lg:!px-0">
+        {officeSpaces.map((office) => (
+          <OfficeSpaceCard key={office.id} officeSpace={office} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
-export default OfficeSection
+export default OfficeSection;
