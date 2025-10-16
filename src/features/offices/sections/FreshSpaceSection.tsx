beffbe66779d2c5fbdf3329ca332cb4439d6052e
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2025-10-16 16:14:24 +0700

    style: adjust heading text size and padding for improved layout in FreshSpaceSection

--- src/features/offices/sections/FreshSpaceSection.tsx
+++ src/features/offices/sections/FreshSpaceSection.tsx
@@ -6,12 +6,12 @@ import { officeSpaces } from '../data/officeSpaces.mock';
 const OfficeSection = () => {
   return (
     <section id="Fresh-Space" className="flex flex-col gap-[30px] w-full max-w-[1130px] mx-auto mt-[100px] mb-[120px]">
-      <h2 className="font-bold text-[32px] leading-[48px] text-nowrap text-center">
+      <h2 className="font-bold text-[28px] sm:text-[32px] leading-tight sm:leading-[48px] text-center">
         Browse Our Fresh Space.
         <br />
         For Your Better Productivity.
       </h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[30px] px-20 sm:px-10 lg:!px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-[30px] px-6 lg:!px-0">
         {officeSpaces.map((office) => (
           <OfficeSpaceCard key={office.id} officeSpace={office} />
         ))}
