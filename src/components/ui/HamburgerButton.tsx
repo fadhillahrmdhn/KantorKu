be3d6e34e9907e19b0405821b8e9062d4e4dc851
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2025-10-16 11:28:23 +0700

    style: update HamburgerButton component for improved animation and positioning of menu icon

--- src/components/ui/HamburgerButton.tsx
+++ src/components/ui/HamburgerButton.tsx
@@ -8,9 +8,9 @@ type HamburgerButtonProps = {
 const HamburgerButton = ({ isMenuOpen, onClick }: HamburgerButtonProps) => {
   return (
     <button type="button" onClick={onClick} className="flex flex-col justify-around w-6 h-6 bg-transparent border-none cursor-pointer p-0 z-20 md:hidden" aria-label="Toggle menu">
-      <div className={`w-6 h-0.5 bg-[#000929] rounded-md transition-all duration-300 ${isMenuOpen ? 'rotate-45 translate-y-[5px]' : ''}`} />
+      <div className={`w-6 h-0.5 bg-[#000929] rounded-md transition-all duration-300 ${isMenuOpen ? 'rotate-45 !translate-y-[8px]' : ''}`} />
       <div className={`w-6 h-0.5 bg-[#000929] rounded-md transition-all duration-300 ${isMenuOpen ? 'opacity-0' : 'opacity-100'}`} />
-      <div className={`w-6 h-0.5 bg-[#000929] rounded-md transition-all duration-300 ${isMenuOpen ? '-rotate-45 -translate-y-[5px]' : ''}`} />
+      <div className={`w-6 h-0.5 bg-[#000929] rounded-md transition-all duration-300 ${isMenuOpen ? '-rotate-45 !-translate-y-[8px]' : ''}`} />
     </button>
   );
 };
