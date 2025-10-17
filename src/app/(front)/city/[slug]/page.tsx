dafe94b1c0d32f3b43b6c68b9e4e5866724ec01c
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2025-10-17 07:42:25 +0700

    style: enhance layout and responsiveness of Hero section in CityDetailPage

--- src/app/(front)/city/[slug]/page.tsx
+++ src/app/(front)/city/[slug]/page.tsx
@@ -75,12 +75,12 @@ const CityDetailPage= async ({ params }: Props) => {
     <>
     <Navbar/>
   <header className="flex flex-col w-full">
-    <section id="Hero-Banner" className="relative flex h-[434px]">
+    <section id="Hero-Banner" className="relative flex h-[434px] px-10 lg:!px-0">
       <div
         id="Hero-Text"
-        className="relative flex flex-col w-full max-w-[650px] h-fit rounded-[30px] border border-[#E0DEF7] p-10 gap-[30px] bg-white mt-[70px] ml-[calc((100%-1130px)/2)] z-10"
+        className="relative flex flex-col w-full max-w-[650px] h-fit rounded-[30px] border border-[#E0DEF7] p-6 md:!p-10 gap-5 md:gap-[30px] bg-white mt-10 md:mt-[70px] mx-auto lg:!ml-[calc((100%-1130px)/2)] z-10"
       >
-        <h1 className="font-extrabold text-[50px] leading-[60px]">
+        <h1 className="font-extrabold text-[32px] md:!text-[50px] leading-[40px] md:!leading-[60px]">
           Great Office in <br />{" "}
           <span className="text-[#0D903A]">{city.name} City</span>
         </h1>
@@ -91,7 +91,7 @@ const CityDetailPage= async ({ params }: Props) => {
       </div>
       <div
         id="Hero-Image"
-        className="absolute right-0 w-[calc(100%-((100%-1130px)/2)-305px)] h-[434px] rounded-bl-[40px] overflow-hidden"
+        className="absolute right-0 w-full lg:!w-[calc(100%-((100%-1130px)/2)-305px)] h-[434px] rounded-bl-[40px] overflow-hidden"
       >
         <Image
           width={980}
