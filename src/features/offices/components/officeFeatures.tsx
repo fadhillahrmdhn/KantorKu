bbe187ce58ef3bfc9271db2cc9f3cf86598c7382
fadhillahrmdhn <fadhillahrmdhn2001@gmail.com>
2025-10-18 14:32:35 +0700

    style: update grid layout and spacing in OfficeFeatures component for improved responsiveness

--- src/features/offices/components/officeFeatures.tsx
+++ src/features/offices/components/officeFeatures.tsx
@@ -36,15 +36,15 @@ const FEATURES_MAP = [
 
 const OfficeFeatures = ({ features }: { features: string[] }) => {
   return (
-    <div className="grid grid-cols-3 gap-x-5 gap-y-[30px]">
+    <div className="grid grid-cols-2 md:!grid-cols-3 gap-x-5 gap-y-[30px]">
       {features.map((feature) => {
         const matched = FEATURES_MAP.find((f) => f.label === feature);
         if(!matched) return null;
           return (
-            <div key={feature} className="flex items-center gap-4">
-              <Image width={34} height={34} src={matched.icon} alt={`${matched.label}icon`} />
-              <div className="flex flex-col gap-[2px]">
-                <p className="font-bold text-lg leading-[24px]">{matched.label}</p>
+            <div key={feature} className="flex items-center">
+              <Image width={34} height={34} src={matched.icon} alt={`${matched.label} icon`}  />
+              <div className="flex flex-col gap-[2px] ml-1">
+                <p className="font-bold text-base sm:text-lg leading-tight sm:leading-[24px]">{matched.label}</p>
                 <p className="text-sm leading-[21px]">{matched.description}</p>
               </div>
             </div>
